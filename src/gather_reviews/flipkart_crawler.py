278238a07eec9a5b06140a3d074f842ba60f2961
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2019-10-26 00:22:46 +0800

    Add code to get num pages of reviews

--- src/gather_reviews/flipkart_crawler.py
+++ src/gather_reviews/flipkart_crawler.py
@@ -8,8 +8,6 @@ from review_template_filler import ReviewTemplateFiller
 
 # url="https://www.flipkart.com/samsung-super-6-108cm-43-inch-ultra-hd-4k-led-smart-tv/p/itmfdzq6khv2pcvz"
 url="https://www.flipkart.com/samsung-galaxy-a70-black-128-gb/product-reviews/itmffhc79jwkwzrw?pid=MOBFFHC7GQZCF8YM"
-num_pages=3
-
 
 if "reviews" not in url:
     print("The URL is not related to product reviews")
@@ -21,6 +19,16 @@ html_parse_spec = utils.get_json_file_contents_as_dict("parse_specs/flipkart_rev
 response = requests.get(url)
 print(f"Response code: {response.status_code}")
 html_soup = BeautifulSoup(response.text, 'html.parser')
+
+num_pages = 1
+navigation_details = html_soup.find("div", class_ = "_2zg3yZ _3KSYCY")
+if navigation_details:
+    num_pages_span = navigation_details.find("span")
+    if num_pages_span:
+        num_pages_text = num_pages_span.text
+        num_pages = int(num_pages_text.replace("Page 1 of", "").strip())
+
+print(f"Number of pages for this product is {num_pages}")
 
 # Get reviews from the first page
 all_product_reviews = []
@@ -42,5 +50,5 @@ print("Number of reviews retrieved")
 print(len(all_product_reviews))
 
 generated_file_name = utils.get_product_name_from_url(url)
-utils.create_json_file_from_dict("generated/" + generated_file_name + ".json", all_product_reviews)
+utils.create_json_file_from_dict("../data/reviews_received/" + generated_file_name + ".json", all_product_reviews)
 
